7a1f2d231a11bf75ff87aa52792349d3d1bb016d
reloadercf <elcfm1@gmail.com>
2020-02-07 14:19:28 -0600

    Elementos definidos

---
+++ imprimir.js
@@ -0,0 +1,7 @@
+const sobreMi={
+    primero:"Soy ingeniero en sistemas",
+    segundo:"Se programar en JavaScript"
+}
+
+document.getElementById('primero').innerHTML=sobreMi.primero
+document.getElementById('segundo').innerHTML=sobreMi.segundo
